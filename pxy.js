2fb10836c617097f4ee4bb7376f45b09b88152a0
Ates Goral <ates.g@myplanetdigital.com>
2015-03-12 20:36:27 -0400

    Initial implementation

--- pxy.js
+++ pxy.js
@@ -9,5 +9,41 @@
         factory((root.pxy = {}));
     }
 }(this, function (exports) {
+    function Pxy(Q) {
+        var originalPromises = [],
+            isValid = true;
 
+        this.invalidate = function (cancel) {
+            isValid = false;
+            originalPromises.forEach(cancel);
+        };
+
+        this.proxy = function (promise) {
+            originalPromises.push(promise);
+
+            var proxy = Q.defer();
+
+            promise.then(function () {
+                originalPromises.splice(originalPromises.indexOf(promise), 1);
+
+                if (isValid) {
+                    proxy.resolve.apply(proxy, arguments);
+                }
+            }, function () {
+                originalPromises.splice(originalPromises.indexOf(promise), 1);
+
+                if (isValid) {
+                    proxy.reject.apply(proxy, arguments);
+                }
+            }, function () {
+                if (isValid) {
+                    proxy.notify.apply(proxy, arguments);
+                }
+            });
+
+            return proxy.promise;
+        };
+    }
+
+    return Pxy;
 }));
